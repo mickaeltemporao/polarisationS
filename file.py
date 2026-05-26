00d4b3933744486f8fa30b85017fe1d13ed47785
Mickael Temporão <16172142+mickaeltemporao@users.noreply.github.com>
2026-05-26 09:50:53 +0200

    minor code review and cleanup

--- file.py
+++ file.py
@@ -3,43 +3,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-df = pd.read_csv("ESS11e04_1/ESS11e04_1.csv")
-
-df_fr = df.loc[df["cntry"] == "FR"]
-df_fr = df_fr[df_fr['lrscale'].between(0, 10)]
-
-df_fr["lrscale"] = abs(df_fr["lrscale"]-5)
-df_fr["lrscale"].hist()
-plt.xlabel("Position gauche-droite")
-plt.title("Distribution du positionnement politique en France")
-plt.show()
-
-df_fr["lrscale"].describe()
-
+df = pd.read_csv("data/raw/ESS11e04_1.csv")
 pol = {}
 var = {}
 
 
-
 ####################################################################
-# Polarisation classique
+# Polarisation Idéologique
 ####################################################################
-for pays in df["cntry"].unique():
-    df_pays = df.loc[df["cntry"] == pays]
-    df_pays = df_pays[df_pays['lrscale'].between(0, 10)]
-    # df_pays["lrscale"] = abs(df_pays["lrscale"]-5)               
-    # print(f"Mean for {pays}: {df_pays['lrscale'].mean()}")
-    pol[pays] = df_pays['lrscale'].mean()
-    var[pays] = df_pays['lrscale'].std()
+df = df[df["lrscale"].between(0,10)]
 
-pol
-var
-
-all = pd.DataFrame.from_dict(pol, orient="index", columns=["Polarisation"])
-all = all.sort_values("Polarisation")
+lrscale = df.groupby("cntry")['lrscale'].mean()
+polar_idl_std = df.groupby("cntry")['lrscale'].std()
 
 
-all.plot(kind="bar")
+lrscale.plot(kind="bar")
 plt.xlabel("Pays")
 plt.ylabel("Polarisation")
 plt.title("Polarisation politique par pays")
@@ -47,111 +25,66 @@ plt.show()
 
 ####################################################################
 
-
-all = pd.DataFrame.from_dict(
-    pol,
-    orient="index",
-    columns=["Polarisation"]
-)
-
-all["Ecart_type"] = pd.Series(var)
-
+lrscale = pd.concat([lrscale, polar_idl_std], axis=1)
+lrscale.columns = ["mean", "std"]
 # Remettre les pays comme colonne
-all = all.reset_index()
+lrscale = lrscale.reset_index()
 
 # Renommer
-all.columns = ["Pays", "Polarisation", "Ecart_type"]
 
 # Trier par polarisation
-all = all.sort_values("Polarisation")
+all = lrscale.sort_values("mean")
 
 
 plt.figure(figsize=(10, 12))
-
 plt.errorbar(
-    x=all["Polarisation"],      # moyenne
-    y=all["Pays"],              # pays
-    xerr=all["Ecart_type"],     # écart-type
+    x=all["mean"],      # moyenne
+    y=all["cntry"],              # pays
+    xerr=all["std"],     # écart-type
     fmt='o',                    # point
     color='blue',
     ecolor='gray',
     elinewidth=4,
     capsize=0
 )
-
-plt.xlabel("Placement Gauche-Droite")
+plt.xlabel("Auto-Placement Gauche-Droite")
 plt.ylabel("Pays")
-plt.title("Polarisation politique par pays")
+plt.title("Auto-Placement Gauche-Droite par pays")
 plt.grid(axis='x', linestyle='--', alpha=0.5)
-
 plt.show()
 
 
 ####################################################################
-# Polarisation en valeur absolue --> valeur de réfénce = 5
+# Polarisation Idéologique en valeur absolue --> valeur de réfénce = 5
 ####################################################################
+df["polar_idl"] = abs(df["lrscale"]-5)
+polar_idl_mean = df.groupby("cntry")['polar_idl'].mean()
+polar_idl_std = df.groupby("cntry")['polar_idl'].std()
 
-
-for pays in df["cntry"].unique():
-    df_pays = df.loc[df["cntry"] == pays]
-    df_pays = df_pays[df_pays['lrscale'].between(0, 10)]
-    df_pays["lrscale"] = abs(df_pays["lrscale"]-5)
-    # print(f"Mean for {pays}: {df_pays['lrscale'].mean()}")
-    pol[pays] = df_pays['lrscale'].mean()
-    var[pays] = df_pays['lrscale'].std()
-
-pol
-var
-
-all = pd.DataFrame.from_dict(pol, orient="index", columns=["Polarisation"])
-all = all.sort_values("Polarisation")
-
-
-all.plot(kind="bar")
+polar_idl_mean.plot(kind="bar")
 plt.xlabel("Pays")
-plt.ylabel("Polarisation")
-plt.title("Polarisation en valeur absolue par pays")
+plt.ylabel("Polarisation Idéologique")
+plt.title("Polarisation Idéologique par pays")
 plt.show()
 
-
-
-all = pd.DataFrame.from_dict(
-    pol,
-    orient="index",
-    columns=["Polarisation"]
-)
-
-all["Ecart_type"] = pd.Series(var)
-
-# Pays comme colonne
-all = all.reset_index()
-
-# Renommer
-all.columns = ["Pays", "Polarisation", "Ecart_type"]
-
-all = all.sort_values("Polarisation")
-
-
+polar_idl = pd.concat([polar_idl_mean, polar_idl_std], axis=1)
+polar_idl.columns = ['mean', 'std']
+polar_idl = polar_idl.reset_index()
+polar_idl = polar_idl.sort_values("mean")
 
 plt.figure(figsize=(10, 12))
-
 plt.errorbar(
-    x=all["Polarisation"],      # moyenne
-    y=all["Pays"],              # pays
-    xerr=all["Ecart_type"],     # écart-type
+    x=polar_idl["mean"],      # moyenne
+    y=polar_idl["cntry"],              # pays
+    xerr=polar_idl["std"],     # écart-type
     fmt='o',                    # point
     color='blue',
     ecolor='gray',
     elinewidth=4,
     capsize=0
 )
-
-plt.xlabel("Ecart à la position centrale (5)")
+plt.xlabel("Polarisation Idéologique")
 plt.ylabel("Pays")
-plt.title("Polarisation en valeur absolue par pays")
+plt.title("Polarisation Idéologique")
 plt.grid(axis='x', linestyle='--', alpha=0.5)
-
 plt.show()
-####################################################################
-####################################################################
-####################################################################
